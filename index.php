460c83c1c6765802821b258c6bc152894b8a37de
Pulkit Bhardwaj <pulkitbhardwaj1994@gmail.com>
2018-09-03 13:49:15 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+	echo "Hello World Wjhats up?";
+
+?>
+
+
+<!doctype html>
+<html>
+
+
+<body>
+<div class="card col-md-4"> <img class="card-img-top" src="images/card-img.png" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a> </div>
+</div>
+</body>
+</html>
